3341dcb8b5efd4c73cde4af86d358c20fc88eabb
AbdoullahBougataya <sience.story@gmail.com>
2024-06-09 12:41:48 +0000

    Sun, Jun 9, 2024, 3:41 PM +03:00

--- RPS/rps.c
+++ RPS/rps.c
@@ -8,7 +8,7 @@ int main(void)
     char computerInput;
     int zot;
     printf("player ( r: rock / p: paper / s: scissors ): ");
-    scanf("%c \n", &playerInput);
+    scanf("%[^\n]c", playerInput);
     srand(time(0));
     zot = rand() % 3;
     computerInput = (zot == 0)? 114:computerInput;
